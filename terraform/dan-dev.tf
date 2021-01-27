7dbf1d2e8d8338eb0b88b08e17a6db6108bccb01
Dan Bernardić <dan.bernardic@gmail.com>
2021-01-27 14:34:34 +0000

    Some initial facilities

---
+++ terraform/dan-dev.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  profile = "default"
+  region  = "ca-central-1"
+}
+
+resource "aws_instance" "dan-dev" {
+  ami             = "ami-02e44367276fe7adc"
+  instance_type   = "t2.micro"
+  key_name        = "bwah-key"
+  security_groups = ["default", "dan-dev"]
+}
+
+resource "aws_key_pair" "bwah" {
+  key_name   = "bwah-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBVfC5ynbR6S7hgVe/y3urWoxcK59SEztVu4U9SsJoTT1dF175cRZkoyPnMValGTO9hhp4q/6xHyeUiaKoSGWtzx5WW58+YV+Gdyck0XnrHC0WcpC8i9q0jTA6oEOyH+FXlnQp6Ym7CpiZauc9xb3m8Ybg3aa6Ds/pUV0TP+FK5p6hLMMk++dtpDTr34wwaBH/nL7DPIRybWsWxBxPRRv6w5T02CToi9NSWzgucRdbj1ER7/GEgRyjL3spM/DWhYxwgbhwBPdWwr12/KqxcvAFeN4jrwoblKyZNckvdG9UYADeopYJ8oGYM4VZrC5PrOA4nT60uzGUnMT95yBx7vox"
+}
